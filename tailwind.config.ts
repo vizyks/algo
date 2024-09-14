12e864ef7dd9f245af0ac88180d826a6f18fa6a1
Joseph Dorman <JDorman555@yahoo.com>
2024-09-13 20:28:46 -0500

    feat: import fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      black: "#1e1e1e",
+      grey: "#262626",
+      "grey-light": "#363636",
+      white: "#DADADA",
+    },
+    fontFamily: {
+      krub: ["Krub", "sans-serif"],
+    },
   },
   plugins: [],
 } satisfies Config;
