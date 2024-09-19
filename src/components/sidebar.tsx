60fbb8a2d44d0885588dd691ab678dd23a860249
Joseph Dorman <JDorman555@yahoo.com>
2024-09-18 19:27:13 -0500

    feat: create sidebar component

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,15 @@
+type SidebarStyle = "left" | "right";
+
+export const Sidebar = ({
+  style,
+  children,
+}: {
+  children: React.ReactNode;
+  style: SidebarStyle;
+}) => {
+  if (style === "right") {
+    return <div className="max-w-[350px] w-full bg-grey">{children}</div>;
+  } else {
+    return <div className="max-w-[350px] w-full bg-grey px-4">{children}</div>;
+  }
+};
