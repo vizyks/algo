7cdec9666fab4809313d12d2773accf1044ea359
Joseph Dorman <JDorman555@yahoo.com>
2024-10-14 16:29:24 -0500

    fix: extend color pallete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,13 +3,14 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      black: "#1e1e1e",
-      "grey-dark": "#2f2f2f",
-      grey: "#262626",
-      "grey-light": "#363636",
-      white: "#DADADA",
+    extend: {
+      colors: {
+        black: "#1e1e1e",
+        "grey-dark": "#2f2f2f",
+        grey: "#262626",
+        "grey-light": "#363636",
+        white: "#DADADA",
+      },
     },
     fontFamily: {
       krub: ["Krub", "sans-serif"],
