f65e4f207f6b6b8df3b06ca0f8dfb7ea550b7ef0
Joseph Dorman <JDorman555@yahoo.com>
2024-11-01 16:40:45 -0500

    feat: typecheck algorithm options

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,22 @@
 export type SortingAlgorithmType =
   | "Bubble Sort"
   | "Selection Sort"
-  | "Insertion Sort";
+  | "Insertion Sort"
+  | "Merge Sort"
+  | "Quick Sort"
+  | "Cycle Detection"
+  | "Nth Factorial"
+  | "Fibonacci Sequence";
+
+export interface SortingAlgorithmOption {
+  cat: string;
+  types: string[];
+}
+
+export interface SortingAlgorithmOptions {
+  bruteForce: SortingAlgorithmOption;
+  divideAndConquer: SortingAlgorithmOption;
+  recursive: SortingAlgorithmOption;
+}
 
 export type AnimationArrayType = [number[], boolean][];
