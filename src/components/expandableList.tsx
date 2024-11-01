a4d226441851e11da460d715c67710591c8407e9
Joseph Dorman <JDorman555@yahoo.com>
2024-11-01 16:43:29 -0500

    fix: add support for new algorithm data/options

--- src/components/expandableList.tsx
+++ src/components/expandableList.tsx
@@ -1,14 +1,10 @@
 import arrowRight from "../assets/arrowRight.svg";
 import arrowDown from "../assets/arrowDown.svg";
 import { useState } from "react";
-import { SortingAlgorithmType } from "../lib/types";
-
-interface algoRec {
-  categories: { catName: string; types: object[] }[];
-}
+import { SortingAlgorithmType, SortingAlgorithmOptions } from "../lib/types";
 
 export default function expandableList(
-  lists: algoRec,
+  list: SortingAlgorithmOptions,
   selectedAlgorithm: SortingAlgorithmType,
   setAlgo: (algo: SortingAlgorithmType) => void
 ) {
@@ -24,46 +20,42 @@ export default function expandableList(
 
   let divs = [];
 
-  for (let i = 0; i < lists.categories.length; i++) {
+  for (const value of Object.values(list)) {
     divs.push(
-      <>
-        <div key={lists.categories[i].catName} className="flex flex-col">
-          <button
-            onClick={(e) => toggleLists(lists.categories[i].catName)}
-            className={`flex w-full justify-between items-center p-2 ${
-              expandedList?.includes(lists.categories[i].catName)
-                ? "bg-grey-light font-bold"
-                : "bg-grey hover:bg-grey-light hover:font-bold"
-            }`}
-          >
-            <h3> {lists.categories[i].catName} </h3>
-            <img
-              src={
-                expandedList?.includes(lists.categories[i].catName)
-                  ? arrowDown
-                  : arrowRight
-              }
-            ></img>
-          </button>
-        </div>
-        {expandedList?.includes(lists.categories[i].catName) && (
+      <div key={value.cat} data-id={value.cat} className="flex flex-col">
+        <button
+          onClick={(e) => toggleLists(value.cat)}
+          className={`flex w-full justify-between items-center p-2 ${
+            expandedList?.includes(value.cat)
+              ? "bg-grey-light font-bold"
+              : "bg-grey hover:bg-grey-light hover:font-bold"
+          }`}
+        >
+          <h3> {value.cat} </h3>
+          <img
+            src={expandedList?.includes(value.cat) ? arrowDown : arrowRight}
+          ></img>
+        </button>
+        {expandedList?.includes(value.cat) && (
           <div className="flex flex-col">
-            {lists.categories[i].types.map((item: SortingAlgorithmType) => (
-              <button
-                onClick={() => setAlgo(item.name)}
-                key={item.name}
-                className={`py-2 px-4 text-left ${
-                  selectedAlgorithm === item.name
-                    ? "bg-grey-dark"
-                    : "hover:bg-grey-dark"
-                }`}
-              >
-                {item.name}
-              </button>
-            ))}
+            {value.types.map((item: SortingAlgorithmType) => {
+              return (
+                <button
+                  onClick={() => setAlgo(item)}
+                  key={item}
+                  className={`py-2 px-4 text-left ${
+                    selectedAlgorithm === item
+                      ? "bg-grey-dark"
+                      : "hover:bg-grey-dark"
+                  }`}
+                >
+                  {item}
+                </button>
+              );
+            })}
           </div>
         )}
-      </>
+      </div>
     );
   }
 
