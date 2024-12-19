3a3c519d1216adc865e2a071691d9839d263316b
Joseph Dorman <JDorman555@yahoo.com>
2024-12-19 12:58:55 -0600

    feat: home button to redirect

--- src/pages/sorting/sidebarLeft.tsx
+++ src/pages/sorting/sidebarLeft.tsx
@@ -2,6 +2,7 @@ import { Sidebar } from "../../components/sidebar";
 import expandableList from "../../components/expandableList";
 import { useSortingAlgorithmContext } from "../../context/Visualizer";
 import { sortingAlgorithmOptions } from "../../lib/utils";
+import { Link } from "react-router-dom";
 
 export function SidebarLeft() {
   const { selectedAlgorithm, setSelectedAlgorithm } =
@@ -9,9 +10,11 @@ export function SidebarLeft() {
 
   return (
     <Sidebar style={"left"}>
-      <h1 className="text-3xl text-center uppercase border-b-2 border-grey-light py-6">
-        ALGO<span className="krub-bold">IN</span>SIGHT
-      </h1>
+      <Link to="/">
+        <h1 className="text-3xl text-center uppercase border-b-2 border-grey-light py-6">
+          ALGO<span className="krub-bold">IN</span>SIGHT
+        </h1>
+      </Link>
       <h2 className="text-lg text-center py-6">Sorting Algorithms</h2>
       <div className="flex flex-col text-sm">
         {expandableList(
