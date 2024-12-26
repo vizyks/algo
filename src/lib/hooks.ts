a0090ecad02291a6e462c2d845c0b8b9d05709d1
Joseph Dorman <JDorman555@yahoo.com>
2024-12-26 15:05:34 -0600

    docs: remove comments

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -4,12 +4,11 @@ export function useToggle(defaultValue?: boolean): [boolean, () => void] {
   // defaultValue if let undefined will turn true because of !!
   const [value, setValue] = useState<boolean>(!!defaultValue);
 
-  // resets state to default between different algorithms
   const toggle = () => {
     setValue(!value);
   };
 
-  /* keeps state between different algorithms
+  /* 
   const toggle = useCallback(() => {
     setValue((x) => !x);
   }, []);
