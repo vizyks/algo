cc93052a7171f1429dcd78290ef95b9233f8a479
Joseph Dorman <JDorman555@yahoo.com>
2024-12-28 22:30:46 -0600

    feat: increase visibility of right sidebar content

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -8,7 +8,7 @@ export const Sidebar = ({
   style: SidebarStyle;
 }) => {
   if (style === "right") {
-    return <div className="max-w-[350px] w-full bg-grey">{children}</div>;
+    return <div className="max-w-[400px] w-full bg-grey">{children}</div>;
   } else {
     return <div className="max-w-[350px] w-full bg-grey px-4">{children}</div>;
   }
