a790418b98a03e973bf9f1ed3f4aa5cfea9951b8
Joseph Dorman <JDorman555@yahoo.com>
2024-12-28 22:33:56 -0600

    feat: custom scrollbar for code view

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Router from "./router.tsx";
 import "./styles/globals.css";
+import "./styles/scrollbar.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
